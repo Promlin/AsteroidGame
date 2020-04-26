ee5ce759bcf0cefb58739d7c02fc90899f82693f
Irina <irinatyumyantseva@gmail.com>
2020-04-26 14:49:28 +0300

    Описание абстрактного класса Logger

---
+++ Tests/ConsoleTest/Loggers/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.Loggers
+{
+    internal abstract class Logger
+    {
+        public abstract void Log(string Message);
+        public void LogInformation(string Message)
+        {
+            Log($"{DateTime.Now:s}[info]:{Message}");
+        }
+        public void LogWarning(string Message)
+        {
+            Log($"{DateTime.Now:s}[warn]:{Message}");
+        }
+        public void LogError(string Message)
+        {
+            Log($"{DateTime.Now:s}[error]:{Message}");
+        }
+    }
+}
--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -10,23 +10,15 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            //Player player1 = new Player();  обращение при отстутствии конструктора с параметрами
-            //player1.Name = "Алёша";
-            Player player = new Player("Иванушка");
-            Console.WriteLine(player.GetName());
+            //Logger log = new Logger();
 
-            Vector2D vector1 = new Vector2D(5, 7);
-            Vector2D vector2 = new Vector2D(-7, 2);
-
-            Vector2D vector3 = vector1 + vector2;
-            double Length = vector3;
-
-            Printer printer = new Printer();
-            printer.Print("Ну как же так");
-            PrefixPrinter prefix_printer = new PrefixPrinter();
-            prefix_printer.PrintData(17.48);
+            //log.LogInformation("Info message");
+            //log.LogWarning("Warning message");
+            //log.LogError("Error message");
 
             Console.ReadLine();
         }
     }
+
+    
 }
