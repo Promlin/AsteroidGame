d1b1cc4918378b25d046853b85555bd4d88cb0ae
Irina <irinatyumyantseva@gmail.com>
2020-04-26 15:45:05 +0300

    Логгер для системы трассировки событий

--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ConsoleTest
 {
@@ -13,7 +14,11 @@ namespace ConsoleTest
         {
             //Logger log = new TextFileLogger("text.log");
             //Logger log = new ConsoleLogger();
-            Logger log = new DebugOutputLogger();
+            //Logger log = new DebugOutputLogger();
+            Logger log = new TraceLogger();
+
+            Trace.Listeners.Add(new TextWriterTraceListener("logger.log"));
+            Trace.Listeners.Add(new XmlWriterTraceListener("logger.log.xml"));
 
             log.LogInformation("Info message");
             log.LogWarning("Warning message");
