3c169aa9554385cc40bc7e49970503750b934c7a
Irina <irinatyumyantseva@gmail.com>
2020-05-03 17:04:45 +0300

    Реализация интерфейсов IComparable, IEquatable

--- Tests/ConsoleTest/Student.cs
+++ Tests/ConsoleTest/Student.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleTest
 {
-    internal class Student
+    internal class Student : IComparable<Student>, IEquatable<Student>
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -14,6 +14,47 @@ namespace ConsoleTest
         public string Patronimyc { get; set; }
         public int GroupID { get; set; }
 
+        public List<int> Ratings { get; set; } = new List<int>();
         
+        public double AverageRating
+        {
+            get
+            {
+                var result = 0;
+                foreach (var rating in Ratings)
+                {
+                    result += rating;
+                }
+                return result / Ratings.Count;
+            }
+        }
+
+        public int CompareTo(Student other)
+        {
+            var current_average_rating = AverageRating;
+            var other_average_rating = other.AverageRating;
+
+            if(Math.Abs(current_average_rating - other_average_rating) < 0.001)
+            {
+                return 0;
+            }
+            if(current_average_rating > other_average_rating)
+            {
+                return +1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public bool Equals(Student other)
+        {
+            if (other == null) return false;
+
+            return Name == other.Name && Surname == other.Surname && Patronimyc == other.Patronimyc;
+
+            return false;
+        }
     }
 }
