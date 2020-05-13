20461210dc259b167401d2abff39b8c50eca7eb7
Irina <irinatyumyantseva@gmail.com>
2020-05-14 00:59:42 +0300

    Менеджер студентов - предметная область

--- UISamples/StudentsManager/Services/StudentsManager.cs
+++ UISamples/StudentsManager/Services/StudentsManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentsManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,31 @@ namespace StudentsManager.Services
 {
     class StudentsManager
     {
+        public List<StudentsGroup> Groups { get; }
+
+        public StudentsManager()
+        {
+            var student_id = 1;
+            Groups = Enumerable.Range(1, 10)
+                .Select(i => new StudentsGroup
+                {
+                    Id = i,
+                    Students = Enumerable
+                    .Range(1, 10)
+                    .Select(j => new Student
+                    {
+                        Id = student_id,
+                        Name = $"Name {student_id}",
+                        Surname = $"Surname {student_id}",
+                        Patronymic = $"Patronymic {student_id++}"
+                    })
+                    .ToList()
+                })
+                .ToList();
+
+            foreach (var group in Groups)
+                foreach (var student in group.Students)
+                    student.Group = group;
+        }
     }
 }
