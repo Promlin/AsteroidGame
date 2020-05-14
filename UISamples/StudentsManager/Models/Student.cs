b9530ef61db4c3c29bfb2ba0788fe9774e37d90d
Irina <irinatyumyantseva@gmail.com>
2020-05-14 14:23:53 +0300

    Работа с MainWindowViewModels

--- UISamples/StudentsManager/Models/Student.cs
+++ UISamples/StudentsManager/Models/Student.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudentsManager.Models
 {
-    internal class Student
+    internal class Student: INotifyPropertyChanged
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Patronymic { get; set; }
         public StudentsGroup Group { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
---
+++ UISamples/StudentsManager/ViewModels/MainWindowViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using StudentsManager.ViewModels.Base;
+
+namespace StudentsManager.ViewModels.Base
+{
+    class MainWindowViewModels : ViewModels
+    {
+        private string _Title = "Редактор студентов";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        private string _Status = "Готов";
+
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+    }
+}
