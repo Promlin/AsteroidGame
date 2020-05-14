a297fc357a363c440ca5c6fd3de8193bed71d80a
Irina <irinatyumyantseva@gmail.com>
2020-05-14 14:55:15 +0300

    дз6

---
+++ Homework_5/Models/Departments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5.Models
+{
+    class Departments
+    {
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+        public override string ToString() => $"{Name}";
+
+        public static implicit operator string(Departments v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Homework_5/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5.Models
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public string Patronimyc { get; set; }
+        public string Department { get; set; }
+        public string Patronymic { get; internal set; }
+
+        internal IEnumerable<object> Range(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
