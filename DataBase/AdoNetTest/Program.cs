9c4547f288128339f1bc4eb31db13e4574151351
Irina <irinatyumyantseva@gmail.com>
2020-05-18 01:14:33 +0300

    Извлечение информации о строке подключения к БД из файла конфигурации

--- DataBase/AdoNetTest/Program.cs
+++ DataBase/AdoNetTest/Program.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace AdoNetTest
 {
@@ -12,14 +13,18 @@ namespace AdoNetTest
     {
         static void Main(string[] args)
         {
-            var connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AsteroidsDB-Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //var connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AsteroidsDB-Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
             //DbConnectionStringBuilder builder = new DbConnectionStringBuilder();
 
             //var sql_server_connection_string_builder = new SqlConnectionStringBuilder(connection_string);
             //sql_server_connection_string_builder.IntegratedSecurity = false;
-            
 
+            const string connection_string_name = "DefaultConnection";
+
+            var connection_string = ConfigurationManager.ConnectionStrings[connection_string_name].ConnectionString;
+
+            Console.ReadLine();
         }
     }
 }
