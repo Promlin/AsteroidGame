b9c3f8f5cfd5ed5ad0302631eb3b9601668b8ccf
Irina <irinatyumyantseva@gmail.com>
2020-05-21 02:31:10 +0300

    сериализация/десериализация в bin & xml

--- FileService/FileHosting/FileHosting/Program.cs
+++ FileService/FileHosting/FileHosting/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace FileHosting
 {
@@ -10,6 +14,42 @@ namespace FileHosting
     {
         static void Main(string[] args)
         {
+            
+
+            Student student = new Student
+            {
+                Name = "Student name",
+                Surname = "Student surname",
+                Birthday = DateTime.Now
+            };
+
+            var binary_formatter = new BinaryFormatter();
+            var xml_serializer = new XmlSerializer(typeof(Student));
+
+            using (var bin_file = File.Create("student.bin"))
+            using (var xml_file = File.Create("student.xml"))
+            {
+                binary_formatter.Serialize(bin_file, student);
+                xml_serializer.Serialize(xml_file, student);
+            }
+
+            Student s1, s2;
+            using(var bin_file = File.Open("student.bin", FileMode.Open))
+            using(var xml_file = File.Open("student.xml", FileMode.Open))
+            {
+                s1 = (Student)binary_formatter.Deserialize(bin_file);
+                s2 = (Student)xml_serializer.Deserialize(xml_file);
+            }
         }
+
+        
+    }
+
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
     }
 }
