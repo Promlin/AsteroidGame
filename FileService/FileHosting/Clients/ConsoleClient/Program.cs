a21c55014802513d643f17d23291e5d4ee249744
Irina <irinatyumyantseva@gmail.com>
2020-05-21 12:25:56 +0300

    Клиент сервиса вручную

--- FileService/FileHosting/Clients/ConsoleClient/Program.cs
+++ FileService/FileHosting/Clients/ConsoleClient/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using FileHosting.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,36 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
+            var client = new FileServiceClient(new BasicHttpBinding(), new EndpointAddress("http://localhost:8080/FileService"));
+
+            var files = client.GetFiles("c:\\GeekBrains");
+
+            foreach(var file in files)
+            {
+                Console.WriteLine(file.FullName);
+            }
+
+            client.StartProcess("calc", "");
+
+            Console.ReadLine();
         }
+    }
+
+    class FileServiceClient : ClientBase<IFileService>, IFileService
+    {
+        public FileServiceClient(Binding binding, EndpointAddress endpoint) : base(binding, endpoint)
+        {
+
+        }
+
+        public DirectoryInfo[] GetDirectories(string Path) => Channel.GetDirectories(Path);
+
+        public DriveInfo[] GetDrives() => Channel.GetDrives();
+
+        public FileInfo[] GetFiles(string Path) => Channel.GetFiles(Path);
+
+        public DateTime GetServiceTime() => Channel.GetServiceTime();
+
+        public int StartProcess(string Path, string Args) => Channel.StartProcess(Path, Args);
     }
 }
